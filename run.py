46e1cb79bcc9d9445d217a59a867a9aad21012a1
Patrick Ogonyo Ochanyo <93025209+Nyakwar-Orera@users.noreply.github.com>
2025-07-31 15:15:31 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,13 +4,13 @@ from app.models import User, Asset, Stationery, Maintenance, Checkout
 app = create_app()
 
 # ⚠️ TEMPORARY: This was only for initial DB reset. Now commented out.
-# def reset_database():
-#     with app.app_context():
-#         db.drop_all()
-#         db.create_all()
-#         print("✅ Database reset complete.")
-#
-# reset_database()
+ def reset_database():
+     with app.app_context():
+        db.drop_all()
+         db.create_all()
+         print("✅ Database reset complete.")
+
+ reset_database()
 
 @app.shell_context_processor
 def make_shell_context():
